e823184a89614c85685d820a094286d4912c54de
Temi Priscilla Jokotola <153321707+temidataspot@users.noreply.github.com>
2025-08-22 21:00:37 +0100

    Delete highrisk_lowrisk.sql

--- highrisk_lowrisk.sql
+++
@@ -1,9 +0,0 @@
-SELECT TOP 20 * FROM features_predictions;
-
-SELECT *
-FROM features_predictions
-WHERE rf_risk_flag = 'High Risk';
-
-SELECT *
-FROM features_predictions
-WHERE rf_risk_flag = 'Low Risk';
